6489ade8f965619ddaf736e5dd67428ce88f5691
Esi Yankey <graceyan244@gmail.com>
2023-05-18 14:42:10 +0000

    fix:fix bugs

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -14,22 +14,22 @@ export const Logo = () => {
   return (
     <div className='logos'>
         <div className="logo-img" data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554c972551243919f425_dropbox-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554c972551243919f425_dropbox-logo.svg" className='logo-images' alt="logos here" />
         </div>
         <div className="logo-img"data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554bfd5fedfcdfb0aaad_zoom-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554bfd5fedfcdfb0aaad_zoom-logo.svg"className='logo-images' alt="logos here" />
         </div>
         <div className="logo-img"data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554b86ff2cd5e536307b_ibm-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554b86ff2cd5e536307b_ibm-logo.svg" className='logo-images' alt="logos here" />
         </div>
         <div className="logo-img"data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554c9725510cdd19f424_docomo-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554c9725510cdd19f424_docomo-logo.svg" className='logo-images' alt="logos here" />
         </div>
         <div className="logo-img"data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554bd4a8140c51c94f26_ucla-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554bd4a8140c51c94f26_ucla-logo.svg" className='logo-images' alt="logos here" />
         </div>
         <div className="logo-img"data-aos="zoom-in">
-            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554b472c7ddf37f41372_rakuten-logo.svg" alt="logos here" />
+            <img src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f1554b472c7ddf37f41372_rakuten-logo.svg" className='logo-images' alt="logos here" />
         </div>
     </div>
   )
