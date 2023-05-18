11ef3013197e4a1bc8e2b4b0cd44778295bfdfbb
Esi Yankey <graceyan244@gmail.com>
2023-05-18 10:56:09 +0000

    refactor: refactor some elements

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,45 +7,45 @@ export const Footer = () => {
       <div className="logo-footer">
         <img src={image} alt="Logo here" className="image1" />
       </div>
-      <div class="footer-layout">
-        <div class="footer-text">why otter.ai</div>
+      <div className="footer-layout">
+        <div className="footer-text">why otter.ai</div>
         <a
           data-tracking-type="button"
           href="https://otter.ai/business?ref=landingfolio"
-          class="footer__link"
+          className="footer__link"
         >
           Otter for Business
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/education?ref=landingfolio"
-          class="footer__link"
+          className="footer__link"
         >
           Otter for Education
         </a>
         <a
           href="https://otter.ai/individuals?ref=landingfolio"
           data-tracking-type="button"
-          class="footer__link"
+          className="footer__link"
         >
           Otter for Individuals
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/pricing?ref=landingfolio"
-          class="footer__link"
+          className="footer__link"
         >
           Pricing
         </a>
       </div>
 
-      <div class="footer-layout">
-        <div class="footer-text">Download</div>
+      <div className="footer-layout">
+        <div className="footer-text">Download</div>
         <a
           data-tracking-type="button"
           href="https://apps.apple.com/us/app/otter-voice-meeting-notes/id1276437113"
           target="_blank"
-          class="footer__link down"
+          className="footer__link down"
         >
           for iOS
         </a>
@@ -53,7 +53,7 @@ export const Footer = () => {
           data-tracking-type="button"
           href="https://play.google.com/store/apps/details?id=com.aisense.otter"
           target="_blank"
-          class="footer__link down"
+          className="footer__link down"
         >
           for Android
         </a>
@@ -61,32 +61,32 @@ export const Footer = () => {
           data-tracking-type="button"
           href="https://chrome.google.com/webstore/detail/otterai-transcribe-record/bnmojkbbkkonlmlfgejehefjldooiedp"
           target="_blank"
-          class="footer__link down"
+          className="footer__link down"
         >
           Chrome Extension
         </a>
       </div>
 
-      <div class="footer-layout">
-        <div class="footer-text">Resources</div>
+      <div className="footer-layout">
+        <div className="footer-text">Resources</div>
         <a
           data-tracking-type="button"
           href="https://otter.ai/blog?ref=landingfolio"
-          class="footer__link res"
+          className="footer__link res"
         >
           Blog
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/careers?ref=landingfolio"
-          class="footer__link res"
+          className="footer__link res"
         >
           Careers
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/press?ref=landingfolio"
-          class="footer__link res"
+          className="footer__link res"
         >
           Press
         </a>
@@ -94,14 +94,14 @@ export const Footer = () => {
           data-tracking-type="button"
           href="https://help.otter.ai/hc/en-us"
           target="_blank"
-          class="footer__link res"
+          className="footer__link res"
         >
           Help &amp; Support
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/media-kit?ref=landingfolio"
-          class="footer__link res"
+          className="footer__link res"
         >
           Media Kit
         </a>
@@ -109,54 +109,54 @@ export const Footer = () => {
           data-tracking-type="button"
           href="https://app.impact.com/campaign-promo-signup/Otterai.brand?execution=e2s1"
           target="_blank"
-          class="footer__link res"
+          className="footer__link res"
         >
           Affiliate
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/privacy-security?ref=landingfolio"
-          class="footer__link res"
+          className="footer__link res"
         >
           Privacy &amp;&nbsp;Security
         </a>
       </div>
 
-      <div class="footer-layout">
+      <div className="footer-layout">
         <a
           href="https://otter.ai/?ref=landingfolio"
           id="footeryear"
           data-tracking-type="button"
           aria-current="page"
-          class="footer__foot-link w--current"
+          className="footer__foot-link w--current"
         >
-          © <span class="footer__year">2023</span> Otter.ai
+          © <span className="footer__year">2023</span> Otter.ai
         </a>
         <a
           href="https://otter.ai/terms-of-service?ref=landingfolio"
           data-tracking-type="button"
-          class="footer__foot-link"
+          className="footer__foot-link"
         >
           Terms of Service
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/privacy-policy?ref=landingfolio"
-          class="footer__foot-link"
+          className="footer__foot-link"
         >
           Privacy Policy
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/jp?ref=landingfolio"
-          class="footer__foot-link"
+          className="footer__foot-link"
         >
           JP
         </a>
         <a
           data-tracking-type="button"
           href="https://otter.ai/zoom?ref=landingfolio"
-          class="footer__foot-link"
+          className="footer__foot-link"
         >
           Zoom
         </a>
--- src/Section6.jsx
+++ src/Section6.jsx
@@ -1,13 +1,12 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import { FaAngleRight } from "react-icons/all";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 export const Section6 = () => {
-
   useEffect(() => {
     AOS.init({
       duration: 1000, // Set the animation duration (in milliseconds)
-      easing: 'ease-in-out', // Set the animation easing
+      easing: "ease-in-out", // Set the animation easing
       once: true, // Specify if animations should only happen once
       // Additional configuration options...
     });
@@ -25,27 +24,33 @@ export const Section6 = () => {
               className="image6"
             />
           </div>
-          <p className="img-text" data-aos="fade-up">
-            Get the most out of your meetings
-            <h6>
+
+          <div className="par-wrap">
+            <p className="img-text" data-aos="fade-up">
+              Get the most out of your meetings
+            </p>
+            <h6 data-aos="fade-up">
               For Bussiness <FaAngleRight />
             </h6>
-          </p>
+          </div>
         </div>
         <div className="image-text">
-          <div className="img-icon green"data-aos="fade-up">
+          <div className="img-icon green" data-aos="fade-up">
             <img
               src="https://assets-global.website-files.com/618e9316785b3582a5178502/61f179e8021779149f1c2891_icon-education.svg"
               alt="graduation"
               className="image6"
             />
           </div>
-          <p className="img-text" data-aos="fade-up">
-            Help students and Faculty succeed
-            <h6>
+
+          <div className="par-wrap">
+            <p className="img-text" data-aos="fade-up">
+              Help students and Faculty succeed
+            </p>
+            <h6 data-aos="fade-up">
               For Education <FaAngleRight />
             </h6>
-          </p>
+          </div>
         </div>
         <div className="image-text">
           <div className="img-icon violet" data-aos="fade-up">
@@ -56,18 +61,22 @@ export const Section6 = () => {
             />
           </div>
 
-          <p className="img-text" data-aos="fade-up" >
-            For all your needs
-            <h6>
+          <div className="par-wrap">
+            <p className="img-text" data-aos="fade-up">
+              For all your needs
+            </p>
+            <h6 data-aos="fade-up">
               For Individuals <FaAngleRight />
             </h6>
-          </p>
+          </div>
         </div>
       </div>
 
-      
-        <img className="right-Otter" src="https://assets-global.website-files.com/618e9316785b3582a5178502/6191777194e4100491e7f318_right-otter.svg" alt="image here" />
-      
+      <img
+        className="right-Otter"
+        src="https://assets-global.website-files.com/618e9316785b3582a5178502/6191777194e4100491e7f318_right-otter.svg"
+        alt="image here"
+      />
     </>
   );
 };
