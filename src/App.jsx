954ac20dcdd975087ee57e45d507a65b12df8360
Esi Yankey <graceyan244@gmail.com>
2023-05-18 09:21:42 +0000

    fix; fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Section3 } from './Section3'
 import { Section4 } from './Section4'
 import { Section5 } from './Section5'
 import { Section6 } from './Section6'
-import { Floating } from './floating'
+import { Floating } from './Floating'
 import { Footer } from './Footer'
 
 
