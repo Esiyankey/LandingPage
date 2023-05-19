38e8ff5ae5ad564cce7bd78f39249a13354f890b
Esi Yankey <graceyan244@gmail.com>
2023-05-19 14:16:43 +0000

    fix:fix bugs

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,31 +1,52 @@
-import {useState} from "react";
-import { FaEquals} from "react-icons/all";
+import { useState } from "react";
+import { FaEquals, FaAngleDown } from "react-icons/all";
 import image from "./assets/image.png";
-import './App.css'
-
-
+import "./App.css";
 
 export const Navbar = () => {
-  const [activeNavbar,SetActiveNavbar]=useState(false)
+  const [activeNavbar, SetActiveNavbar] = useState(false);
 
-  
-  const ChangeBackground =()=>{
-    if(window.scrollY > 50){
-      SetActiveNavbar(true)
+  const ChangeBackground = () => {
+    if (window.scrollY > 50) {
+      SetActiveNavbar(true);
+    } else {
+      SetActiveNavbar(false);
     }
-    else{
-      SetActiveNavbar(false)
-    }
-  }
-  window.addEventListener('scroll',ChangeBackground);
-
-
+  };
+  window.addEventListener("scroll", ChangeBackground);
 
   return (
     <>
-      <div className={activeNavbar ? "Navbar active":'Navbar'}>
-        <div className="logo">
-          <img src={image} alt="Logo here" className="image1"/>
+      <div className={activeNavbar ? "Navbar active" : "Navbar"}>
+        <div className="nav-links">
+          <div className="logo">
+            <img src={image} alt="Logo here" className="image1" />
+          </div>
+          <div className="links">
+            <ul className="ul">
+              <li>
+                Why Otter.ai
+                <FaAngleDown />
+              </li>
+              <li>
+                Pricing
+                <FaAngleDown />
+              </li>
+              <li>
+                Download
+                <FaAngleDown />
+              </li>
+              <li>
+                Resources
+                <FaAngleDown />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="side-links">
+          <li className="contact">Contact Sales</li>
+          <li>Log In</li>
+          <button className="Free">Start For Free</button>
         </div>
         <div className="menu">
           <div className="menu-icons">
@@ -37,7 +58,7 @@ export const Navbar = () => {
             </div>
           </div>
         </div>
-      {/* <DropDown/> */}
+        {/* <DropDown/> */}
       </div>
     </>
   );
